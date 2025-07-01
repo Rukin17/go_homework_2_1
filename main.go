fc945514c5296ca3fe4dd3a3014fc5432eb92908
Andrey Rukin <rukin899@gmail.com>
2025-07-01 09:42:45 +0300

    add error

--- main.go
+++ main.go
@@ -12,9 +12,14 @@ import (
 */
 func main() {
 	var input string
+	var err error
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Введите строку: ")
-	input, _ = reader.ReadString('\n')
+	input, err = reader.ReadString('\n')
+	if err != nil {
+		println("Ошибка ввода", err)
+		return
+	}
 	result := utf8.RuneCountInString(input)
 	fmt.Println((result - 1))
 
